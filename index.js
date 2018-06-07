0b8837f5232e5a8dd0f67dd6ca6f4184c63627c3
aidant <aidan.temple1@gmail.com>
2018-06-07 13:47:38 +1000

    fix file path bug

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const { stringifyRequest } = require('loader-utils')
+const path = require('path')
 
 module.exports = exports = function (content) {
   if (!this.emitFile || typeof this.emitFile !== 'function') {
     throw new Error('this.emitFile is required')
   }
+  const filename = path.parse(this.resourcePath).base
+  this.emitFile(filename, content)
   const filepath = stringifyRequest(this, this.resourcePath)
-  this.emitFile(filepath, content)
   return `
     try {
-      global.process.dlopen(module, '${filepath}')
+      global.process.dlopen(module, ${filepath})
     } catch (error) {
       throw new Error (\`Cannot open ${filepath}: \${error}\`)
     }
